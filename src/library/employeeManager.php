c99b03444141ae7da9599357500eab6fc066c9bc
Pablo Caravaca <pablocaravacacalvo@gmail.com>
2021-02-09 17:43:49 +0100

    getEmployee

--- src/library/employeeManager.php
+++ src/library/employeeManager.php
@@ -1,47 +1,50 @@
 <?php
-/**
- * EMPLOYEE FUNCTIONS LIBRARY
- *
- * @author: Jose Manuel Orts
- * @date: 11/06/2020
- */
+
+define("EMPLOYEES_JSON_PATH", $_SERVER["DOCUMENT_ROOT"] . "/php-employee-management-v1/resources/employees.json");
+
+getEmployee(1);
 
 function addEmployee(array $newEmployee)
 {
-// TODO implement it
+    // TODO implement it
 }
 
 
 function deleteEmployee(string $id)
 {
-// TODO implement it
+    // TODO implement it
 }
 
 
 function updateEmployee(array $updateEmployee)
 {
-// TODO implement it
+    // TODO implement it
 }
 
 
 function getEmployee(string $id)
 {
-// TODO implement it
+    $jsonData = file_get_contents(EMPLOYEES_JSON_PATH);
+    $employee = json_decode($jsonData, true)[$id];
+    return json_encode($employee);
 }
 
 
 function removeAvatar($id)
 {
-// TODO implement it
+    // TODO implement it
 }
 
 
 function getQueryStringParameters(): array
 {
-// TODO implement it
+    // TODO implement it
+    return  array();
 }
 
 function getNextIdentifier(array $employeesCollection): int
 {
-// TODO implement it
+    // TODO implement it
+    return 0;
+
 }
