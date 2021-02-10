e4a4bc0e795d15c9290bd43498782094deae36aa
Pablo Caravaca <pablocaravacacalvo@gmail.com>
2021-02-10 13:15:41 +0100

    add id when add employee

--- src/library/employeeManager.php
+++ src/library/employeeManager.php
@@ -5,18 +5,10 @@ define("EMPLOYEES_JSON_PATH", $_SERVER["DOCUMENT_ROOT"] . "/php-employee-managem
 function addEmployee(array $newEmployee)
 {
     $employees = json_decode(file_get_contents(EMPLOYEES_JSON_PATH), true);
-    $employeeExists = false;
-    foreach ($employees as $employee) {
-        if ($employee['id'] == $newEmployee['id']) {
-            $employeeExists = true;
-            break;
-        }
-    }
-    if (!$employeeExists) {
-        array_push($employees, $newEmployee);
-        $fileData = json_encode(array_values($employees), JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-        file_put_contents(EMPLOYEES_JSON_PATH, $fileData);
-    }
+    $newEmployee['id'] = end($employees)['id'] + 1;
+    array_push($employees, $newEmployee);
+    $fileData = json_encode(array_values($employees), JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+    file_put_contents(EMPLOYEES_JSON_PATH, $fileData);
 }
 
 function deleteEmployee(string $id)
