b56fa72c60f53a0d7cd66b6752df6527f2d3d5d5
Pablo Caravaca <pablocaravacacalvo@gmail.com>
2021-02-10 17:05:16 +0100

    solution

---
+++ src/library/employeeController.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once('./employeeManager.php');
+
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'GET': {
+            $ids = !empty($_GET['ids']) ? explode(',', $_GET['ids']) : [];
+            echo getEmployees($ids);
+            break;
+        }
+    case 'POST': {
+            if (isset($_POST)) {
+                addEmployee($_POST);
+                http_response_code(201);
+            } else {
+                http_response_code(400);
+            }
+            break;
+        }
+    case 'PUT': {
+            $employeeData = file_get_contents('php://input');
+            echo var_dump(json_decode($employeeData, true));
+            updateEmployee(json_decode($employeeData, true));
+            break;
+        }
+    case 'DELETE': {
+            if (isset($_GET['id'])) {
+                echo deleteEmployee($_GET['id']);
+                http_response_code(204);
+            }
+            break;
+        }
+}
---
+++ src/library/employeeHelper.php
@@ -0,0 +1,65 @@
+<?php
+
+function findItemWithId(array &$array, string $id)
+{
+    foreach ($array as $key => &$item) {
+        if ($item['id'] == $id) {
+            return (object) array(
+                'key' => $key,
+                'value' => &$item
+            );
+        }
+    }
+    return false;
+}
+
+function encodeJson($item)
+{
+    return json_encode($item, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+}
+
+function decodeJsonFile(string $path)
+{
+    return json_decode(file_get_contents($path), true);
+}
+
+function saveArrayAsJson(string $path, array $array)
+{
+    $fileData = encodeJson(array_values($array));
+    file_put_contents($path, $fileData);
+}
+
+function array_sort($array, $on, $order = SORT_ASC)
+{
+    $new_array = array();
+    $sortable_array = array();
+
+    if (count($array) > 0) {
+        foreach ($array as $k => $v) {
+            if (is_array($v)) {
+                foreach ($v as $k2 => $v2) {
+                    if ($k2 == $on) {
+                        $sortable_array[$k] = $v2;
+                    }
+                }
+            } else {
+                $sortable_array[$k] = $v;
+            }
+        }
+
+        switch ($order) {
+            case SORT_ASC:
+                asort($sortable_array);
+                break;
+            case SORT_DESC:
+                arsort($sortable_array);
+                break;
+        }
+
+        foreach ($sortable_array as $k => $v) {
+            $new_array[$k] = $array[$k];
+        }
+    }
+
+    return $new_array;
+}
