59fa23017cb531d547b758edb0abe7abd4172acf
Jan Puig <janpuigcapella@gmail.com>
2021-02-10 13:23:14 +0100

    fix format

--- src/library/loginManager.php
+++ src/library/loginManager.php
@@ -1,45 +1,43 @@
 <?php
 session_start();
 
-define( 'USERS_JSON_PATH', $_SERVER[ 'DOCUMENT_ROOT' ] . "/php-employee-management-v1/resources/users.json" );
+define('USERS_JSON_PATH', $_SERVER['DOCUMENT_ROOT'] . "/php-employee-management-v1/resources/users.json");
 
-function getUser( string $userEmail )
+function getUser(string $userEmail)
 {
-    $users = json_decode( file_get_contents( USERS_JSON_PATH ) ) -> users;
+    $users = json_decode(file_get_contents(USERS_JSON_PATH))->users;
 
-    foreach ( $users as $user) {
-        if( $user -> email === $userEmail ){
+    foreach ($users as $user) {
+        if ($user->email === $userEmail) {
             return $user;
         }
     }
 }
 
-function logIn( string $userEmail, string $password ) : bool
+function logIn(string $userEmail, string $password): bool
 {
-    $user = getUser( $userEmail );
+    $user = getUser($userEmail);
 
-    if ( isset( $user ) ){
+    if (isset($user)) {
 
-        $isPasswordCorrect = password_verify( $password, $user -> password );
+        $isPasswordCorrect = password_verify($password, $user->password);
 
-        if( $isPasswordCorrect ){
-            $_SESSION[ 'userId' ] = $user -> userId;
-            $_SESSION[ 'time' ] = time();
-            $_SESSION[ 'lifeTime' ] = 600;
+        if ($isPasswordCorrect) {
+            $_SESSION['userId'] = $user->userId;
+            $_SESSION['time'] = time();
+            $_SESSION['lifeTime'] = 600;
 
             return true;
-
         } else {
             return false;
         }
     } else {
 
         return false;
-
     }
 }
 
-function logOut() :bool
+function logOut(): bool
 {
     return session_destroy();
 }
