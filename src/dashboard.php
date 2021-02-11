85d9baa9987fbf6741ed8b88b9acea685265844d
janpc <64541042+janpc@users.noreply.github.com>
2021-02-11 12:13:42 +0100

    Header (#17)

--- src/dashboard.php
+++ src/dashboard.php
@@ -1,1 +1,19 @@
 <!-- TODO Main view or Employees Grid View here is where you get when logged here there's the grid of employees -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/main.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+    <title>Dashboard</title>
+</head>
+
+<body class='dashboard'>
+    <?php include('../assets/html/header.html'); ?>
+</body>
+
+</html>
--- src/employee.php
+++ src/employee.php
@@ -1,1 +1,19 @@
 <!-- TODO Employee view -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/main.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+    <title>Employee</title>
+</head>
+
+<body class='employee'>
+    <?php include('../assets/html/header.html'); ?>
+</body>
+
+</html>
