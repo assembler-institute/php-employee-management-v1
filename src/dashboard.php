2766f00bdc68509558a527725cbb80c699cdc24a
IGNACIOFC <45830117+IGNACIOFC@users.noreply.github.com>
2021-02-11 13:18:40 +0100

    add bootstrap

--- src/dashboard.php
+++ src/dashboard.php
@@ -12,7 +12,7 @@
     crossorigin="anonymous"></script>
     <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jsgrid/1.5.3/jsgrid.min.css" />
     <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jsgrid/1.5.3/jsgrid-theme.min.css" />
-    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jsgrid/1.5.3/jsgrid.min.js"></script>
     <title>Document</title>
 </head>
@@ -27,6 +27,7 @@
     <script src="../../assets/js/dashboard.js"></script>
 </body>
 </html>
+
 <?php
 
 
