75ba25661ddd86f211348aac498ab59c1994f5cb
Anntroy <ana.troyan@gmail.com>
2021-02-11 19:26:27 +0100

    Changes

--- src/employee.php
+++ src/employee.php
@@ -1,4 +1,16 @@
 <!-- TODO Employee view -->
-<?php
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Employee</title>
+    <link rel="stylesheet" href="../assets/css/employee.css">
+</head>
+<body>
+    <?php
+    include_once('../assets/html/employee.html');
+    ?>
+</body>
+</html>
