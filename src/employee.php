7ac19ceab94bdfabe76d46031eed82acd1bf5008
Anntroy <ana.troyan@gmail.com>
2021-02-12 15:51:49 +0100

    Employee html and css

--- src/employee.php
+++ src/employee.php
@@ -7,6 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Employee</title>
     <link rel="stylesheet" href="../assets/css/employee.css">
+    <link rel="stylesheet" href="../assets/css/main.css">
+    <link rel="stylesheet" href="../assets/css/login.css">
 </head>
 <body>
     <?php
