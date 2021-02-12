e96481b11858c0f5be757c03702c8478b7832f36
Estefanía Cordeiro Brión <estefania.cbrion@gmail.com>
2021-02-12 11:25:50 +0100

    Change of headers to the main php files to login and employee pages

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <!-- TODO Application entry point. Login view -->
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/login.css">
+</head>
+<body class="d-flex flex-column h-100 w-100">
 
-include "./assets/html/login.html";
+    <?php
 
+    include "./assets/html/login.html";
+    echo $_POST["login_form_email"];
+
+    ?>
+
+</body>
+</html>
--- src/employee.php
+++ src/employee.php
@@ -1,6 +1,24 @@
 <!-- TODO Employee view -->
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Employee</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/css/main.css">
+    <link rel="stylesheet" href="../assets/css/employee.css">
+</head>
+<body class="h-100">
 
-include "../assets/html/header.html";
-include "../assets/html/employee.html";
-include "../assets/html/footer.html";
+    <?php
+
+    include "../assets/html/header.html";
+    include "../assets/html/employee.html";
+    include "../assets/html/footer.html";
+
+    ?>
+
+</body>
+</html>
