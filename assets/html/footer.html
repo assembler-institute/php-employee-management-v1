dbac0f9484a1460e45e91edb1f2a2f77f8ddbd53
Mathias Fortuna <mathiasfortuna@hotmail.com>
2021-07-05 12:56:54 +0200

    Finished footer

---
+++ assets/html/footer.html
@@ -0,0 +1,7 @@
+<footer class="footer mt-auto py-3 bg-light">
+  <div class="container footer-container text-center">
+    <span class="text-muted"
+      >Assembler School of Software Engineering 2021</span
+    >
+  </div>
+</footer>
--- src/dashboard.php
+++ src/dashboard.php
@@ -1,26 +1,28 @@
-<?php
-
-include "../assets/html/header.html";
-?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-100">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
-
+    <link rel="stylesheet" href="../assets/css/main.css">
     <title>Dashboard</title>
 </head>
 
-<body>
+<body class="d-flex flex-column h-100">
+    <?php
 
+    include "../assets/html/header.html";
+    ?>
+
+
+
+    <?php
+    include "../assets/html/footer.html";
+    ?>
 </body>
-<script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 
-<?php
-include "../assets/html/footer.html";
-?>
+<script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 
 </html>
