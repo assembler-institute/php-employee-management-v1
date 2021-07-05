32a71f577edcd1e7a43fc459973ce47137750b19
Mathias Fortuna <mathiasfortuna@hotmail.com>
2021-07-05 12:48:39 +0200

    Created header

--- src/dashboard.php
+++ src/dashboard.php
@@ -1,1 +1,26 @@
-<!-- TODO Main view or Employees Grid View here is where you get when logged here there's the grid of employees -->
+<?php
+
+include "../assets/html/header.html";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
+
+    <title>Dashboard</title>
+</head>
+
+<body>
+
+</body>
+<script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+<?php
+include "../assets/html/footer.html";
+?>
+
+</html>
