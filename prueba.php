cccb1c60b70157c4126c0abe3852713b61bd9df7
Nachomontoya <montoyanacho@gmail.com>
2021-07-05 16:13:00 +0200

    render employees

--- prueba.php
+++ prueba.php
@@ -1,6 +1,6 @@
 <?php
 require('conexion_ajax_prueba.php');
-//require("assets/html/header.html");
+
 //session_start();
 ?>
 <!DOCTYPE html>
@@ -16,6 +16,12 @@ require('conexion_ajax_prueba.php');
 
 </head>
 <body>
+    <header>
+    <?php
+    require("assets/html/header.html");
+    ?>
+    </header>
+    <main>
     <?php
         $listEmployees = $_SESSION['listEmployees'];
         foreach ($listEmployees as $key => $list){
@@ -32,46 +38,55 @@ require('conexion_ajax_prueba.php');
                 echo "<hr>";
             }
     ?>
+    </main>
     <div id="jsGrid"></div>
 </body>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jsgrid/1.5.3/jsgrid.min.js"></script>
 <script>
-    var product = [
-        {    
-        "id": "1",
-        "name": "Rack",
-        "lastName": "Lei",
-        "email": "jackon@network.com",
-        "gender": "man",
-        "city": "San Jone",
-        "streetAddress": "126",
-        "state": "CA",
-        "age": "24",
-        "postalCode": "394221",
-        "phoneNumber": "7383627627"
-        },
-    ];
-    $("#jsGrid").jsGrid({
+    const dataPath = "./resources/employees.json";
+    async function getEmployees(dataPath) {
+        let result = await $.getJSON(dataPath);
+        $("#jsGrid").jsGrid({
         width: "100%",
-        height: "400px",
+        height: "auto",
         inserting: true,
         editing: true,
         sorting: true,
         paging: true,
-        data: product,
+        data: result,
         fields: [
-            { name: "id", type: "number" },
+            // { name: "id", type: "number" },
             { name: "name", type: "text", width: 50 },
-            { name: "lastName", type: "text", width: 200 },
-            { name: "email", type: "email", width: 50 },
-            { name: "garder", type: "text", width: 200 },
-            { name: "city", type: "text", width: 50 },
-            { name: "streetAddress", type: "text", width: 50 },
-            { name: "state", type: "text", width: 50 },
+            // { name: "lastName", type: "text", width: 50 },
+            { name: "email", type: "email", width: 100 },
             { name: "age", type: "number", width: 50 },
+            { name: "streetAddress", type: "text", width: 50 },
+            { name: "city", type: "text", width: 50 },
+            { name: "state", type: "text", width: 50 },
             { name: "postalCode", type: "number", width: 50 },
             { name: "phoneNumber", type: "number", width: 50 },
+            // { name: "gender", type: "text", width: 50 },
         ]
     });
+    }
+    
+    getEmployees(dataPath);
+
+    // var product = [
+    //     {    
+    //     "id": "1",
+    //     "name": "Rack",
+    //     "lastName": "Lei",
+    //     "email": "jackon@network.com",
+    //     "gender": "man",
+    //     "city": "San Jone",
+    //     "streetAddress": "126",
+    //     "state": "CA",
+    //     "age": "24",
+    //     "postalCode": "394221",
+    //     "phoneNumber": "7383627627"
+    //     },
+    // ];
+
 </script>
 </html>
