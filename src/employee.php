729f0239cf28a51a5aefed991936c980ec4101c6
Mathias Fortuna <mathiasfortuna@hotmail.com>
2021-07-06 15:33:51 +0200

    Finished employee frontend structure

--- src/employee.php
+++ src/employee.php
@@ -2,7 +2,7 @@
 <?php
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-100">
 
 <head>
     <meta charset="UTF-8">
@@ -20,12 +20,107 @@
 
     <div class="container-fluid">
 
-        <main class="col-lg-10 p-2 mx-auto gy-2">
-            <div>
-                <h2>Employees</h2>
+        <main class="col-lg-8 col-md-10 p-2 mx-auto gy-2">
+            <div class="pb-2">
+                <h2>Employee</h2>
             </div>
-            <div id="jsGrid"></div>
-        </main>
+            <div class="col">
+                <form class="needs-validation" novalidate>
+                    <div class="row g-3">
+                        <div class="col-sm-6">
+                            <label for="firstName" class="form-label">First name</label>
+                            <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
+                            <div class="invalid-feedback">
+                                Valid first name is required.
+                            </div>
+                        </div>
+
+                        <div class="col-sm-6">
+                            <label for="lastName" class="form-label">Last name</label>
+                            <input type="text" class="form-control" id="lastName" placeholder="" value="">
+                            <!-- <div class="invalid-feedback">
+                                Valid last name is required.
+                            </div> -->
+                        </div>
+                        <div class="col-md-8">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="email" class="form-control" id="email" placeholder="user@example.com" required>
+                            <div class="invalid-feedback">
+                                Please enter a valid email address.
+                            </div>
+                            <small class="text-muted">We'll never share this email with anyone else.</small>
+                        </div>
+                        <div class="col-md-4">
+                            <label for="gender" class="form-label">Gender</label>
+                            <select class="form-select" id="gender">
+                                <option value="">Choose...</option>
+                                <option>Female</option>
+                                <option>Male</option>
+                                <option>Other</option>
+                                <option>No answer</option>
+                            </select>
+                            <!-- <div class="invalid-feedback">
+                                Please provide a valid gender.
+                            </div> -->
+                        </div>
+
+                        <div class="col-sm-6">
+                            <label for="city" class="form-label">City</label>
+                            <input type="text" class="form-control" id="city" required>
+                            <div class="invalid-feedback">
+                                Please enter the employee's city.
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <label for="streetAddress" class="form-label">Street Address</label>
+                            <input type="number" class="form-control" id="streetAddress" placeholder="1234" required>
+                            <div class="invalid-feedback">
+                                Please enter the street number.
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <label for="state" class="form-label">State</label>
+                            <input type="text" class="form-control" id="state" placeholder="CA" required>
+                            <div class="invalid-feedback">
+                                Please enter the state.
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <label for="age" class="form-label">Age</label>
+                            <input type="number" class="form-control" id="age" placeholder="22" required>
+                            <div class="invalid-feedback">
+                                Please enter the employee's age.
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <label for="postalCode" class="form-label">Postal Code</label>
+                            <input type="text" class="form-control" id="postalCode" placeholder="394221" required>
+                            <div class="invalid-feedback">
+                                Please enter the employee's postal code.
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <label for="phoneNumber" class="form-label">Phone Number</label>
+                            <input type="number" class="form-control" id="phoneNumber" placeholder="123456789" required>
+                            <div class="invalid-feedback">
+                                Please enter the phone number.
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <!-- <hr class="my-4"> -->
+                    <div class="text-center pt-4">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <button type="button" class="btn btn-secondary">Return</button>
+
+                    </div>
+
+                    <!-- <button class="w-100 btn btn-primary btn-lg" type="submit">Continue to checkout</button> -->
+                </form>
+            </div>
+    </div>
+    </main>
     </div>
 
 
@@ -37,5 +132,6 @@
 <script type="text/javascript" src="../node_modules/jquery/dist/jquery.min.js"></script>
 <script type="text/javascript" src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <script type="text/javascript" src="../node_modules/jsgrid/dist/jsgrid.min.js"></script>
+<script type="text/javascript" src="../assets/js/employee.js"></script>
 
 </html>
