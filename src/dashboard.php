42e567ed8f05a75d2ddc6f50cbdf1021d1fd70f5
Ethan <ctabbx@gmail.com>
2021-07-06 16:41:56 +0200

    Pull

--- src/dashboard.php
+++ src/dashboard.php
@@ -69,25 +69,99 @@ session_start();
             },
 
             fields: [{
-                    name: 'id',
-                    type: 'text',
-                },
-                {
                     name: "name",
                     type: "text",
-                    width: 150,
+                    width: 100,
                     validate: "required"
                 },
                 {
+                    name: "email",
+                    type: "text",
+                    width: 100,
+                    validate: [
+                        "required",
+                        {
+                            message: "Please put a valid email address",
+                            validator: function(value, item) {
+                                return /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(value);
+                            },
+                        }
+                    ]
+                },
+
+                {
                     name: "age",
                     type: "text",
-                    width: 50
+                    width: 50,
+                    validate: {
+                        validator: function(value) {
+                            if (value > 0 && value < 80) {
+                                return true;
+                            }
+                        },
+                        message: function(value, item) {
+                            return "The client age should be between 0 and 80. Entered age is \"" + value + "\" is out of specified range.";
+                        },
+                        param: [0, 80]
+                    }
+                },
+                {
+                    name: 'streetAddress',
+                    type: 'text',
+                    width: '50',
+                    validate: 'required',
+                },
+                {
+                    name: 'city',
+                    type: 'text',
+                    width: '100',
+                    validate: 'required',
+                },
+                {
+                    name: 'state',
+                    type: 'text',
+                    width: '50',
+                    validate: 'required',
+                },
+                {
+                    name: 'postalCode',
+                    type: 'text',
+                    width: '100',
+                    validate: {
+                        validator: function(value) {
+                            if (value.length == 6) {
+                                return true;
+                            }
+                        },
+                        message: "Please enter a valid postal code",
+                    }
+                },
+                {
+                    name: 'phoneNumber',
+                    type: 'text',
+                    width: '100',
+                    validate: {
+                        validator: function(value, item) {
+                            if (value.length < 20) {
+                                return true;
+                            }
+                        },
+                        message: "Please enter a valid phone number",
+                    }
+
                 },
                 {
                     type: "control"
                 }
             ]
         });
+
+        // jsGrid.validators.email = {
+        //     message: "Please enter a valid time, between 00:00 and 23:59",
+        //     validator: function(value, item) {
+        //         return /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(value);
+        //     }
+        // }
 
         // $.ajax({
         //     type: "DELETE",
