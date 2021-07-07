0dd55acd1fc6a20b5e66cc2db31372c98faf084f
Nachomontoya <montoyanacho@gmail.com>
2021-07-07 11:54:10 +0200

    employeeController cleaned up

--- src/library/employeeController.php
+++ src/library/employeeController.php
@@ -4,21 +4,11 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 header("Content-Type: application/json");
 
-// switch ($method) {
-//   case "GET":
-//     getAllEmployees();
-// }
-
-if($method == 'POST') {
-  // var_dump($_POST);
-  $newEmployee = $_POST;
-  echo addEmployee($newEmployee);
+switch ($method) {
+  case 'POST':
+    // var_dump($_POST);
+    $newEmployee = $_POST;
+    echo addEmployee($newEmployee);
 }
-
-// $query = "INSERT INTO sample_data (first_name, last_name) VALUES (:first_name, :last_name)";
-// $statement = $connect->prepare($query);
-// $statement->execute($data);
-//   // addEmployee(array $newEmployee)
-// }
 
 ?>
