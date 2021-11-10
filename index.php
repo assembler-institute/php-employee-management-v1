4fc60cd3a81427f85e708f1e5011cd5ce9e33e27
CarlosVelilla <carlosvelillaf@hotmail.com>
2021-11-10 17:01:44 +0100

    Add first HTML structure

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <!-- TODO Application entry point. Login view -->
+<?php
+
+require_once('./assets/html/header.html');
+
+?>
+
+<div>hola</div>
+
+
+<?php
+
+require_once('./assets/html/footer.html');
+
+?>
