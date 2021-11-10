92a3b2f2bef438ee4973e48df3fb7c62ef83d870
sergi92 <45389112+sergi92@users.noreply.github.com>
2021-11-10 17:20:40 +0100

    employee's page layout

--- index.php
+++ index.php
@@ -2,5 +2,6 @@
 <?php
 require_once("assets/html/header.html");
 // require_once("assets/html/main.html");
-require_once("assets/html/dashboard.html");
+// require_once("assets/html/dashboard.html");
 require_once("assets/html/footer.html");
+require_once("assets/html/employeesPage.html");
