4d1ad3c41d5989d9b2612992f64448e69fdb22a6
Cherrerotinoco <c.herrerotinoco@gmail.com>
2021-11-11 14:18:37 +0100

    Add delete AJAX request

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,77 +4,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
   .then(res => res.json())
   .then(data => setupJsGrid(data))
 
-  function setupJsGrid(data) {
-    $("#jsGrid").jsGrid({
-      width: "100%",
-      height: "400px",
-      
-      inserting: true,
-      editing: true,
-      sorting: true,
-      paging: true,
-
-      onItemInserted: insertNewEntry,
-      onItemUpdated: updateEntry, 
-      onItemDeleted: deleteEntry,
-
-      deleteConfirm: "Do you really want to delete the employee?",
-
-      data: data,
-      
-      fields: [{
-          name: "name",
-          type: "text",
-          validate: "required"
-        },
-        {
-          name: "age",
-          type: "text",
-          validate: "required"
-        },
-        {
-          name: "streetAddress",
-          type: "text",
-          validate: "required"
-        },
-        {
-          name: "city",
-          type: "text",
-          validate: "required",
-          textField: "Name"
-        },
-        {
-          name: "state",
-          type: "text",
-          validate: "required",
-          textField: "Name"
-        },
-        {
-          name: "phoneNumber",
-          type: "text",
-          validate: "required",
-          title: "Phone number"
-        },
-        {
-          type: "control"
-        }
-      ]
-      });
-  }
 });
 
-// Basic CUD for AJAX
+// Create & Delete for AJAX
 function insertNewEntry({item}) {
   makeRequest(item, 'create')
 }
-function updateEntry({item}) {
-  makeRequest(item, 'update')
-}
+
 function deleteEntry({item}) {
   makeRequest(item, 'delete')
 }
+
 function makeRequest(item, action) {
-  fetch('../../resources/library/employeeController.php', {
+  fetch('../../src/library/employeeController.php', {
     method: 'post',
     body: JSON.stringify({
       'item': item,
@@ -84,6 +26,63 @@ function makeRequest(item, action) {
       'Content-Type': 'application/json'
     }
   })
-  .then(res => res.json())
-  .then(data => console.log(data)) // Get backend response 
+  .then(res => cosole.log(res))
+   // Get backend response 
+}
+
+function setupJsGrid(data) {
+  $("#jsGrid").jsGrid({
+    width: "100%",
+    height: "400px",
+    
+    inserting: true,
+    editing: false,
+    sorting: true,
+    paging: true,
+
+    onItemInserted: insertNewEntry,
+    onItemDeleted: deleteEntry,
+
+    deleteConfirm: "Do you really want to delete the employee?",
+
+    data: data,
+    
+    fields: [{
+        name: "name",
+        type: "text",
+        validate: "required"
+      },
+      {
+        name: "age",
+        type: "text",
+        validate: "required"
+      },
+      {
+        name: "streetAddress",
+        type: "text",
+        validate: "required"
+      },
+      {
+        name: "city",
+        type: "text",
+        validate: "required",
+        textField: "Name"
+      },
+      {
+        name: "state",
+        type: "text",
+        validate: "required",
+        textField: "Name"
+      },
+      {
+        name: "phoneNumber",
+        type: "text",
+        validate: "required",
+        title: "Phone number"
+      },
+      {
+        type: "control"
+      }
+    ]
+    });
 }
--- src/library/employeeController.php
+++ src/library/employeeController.php
@@ -1,5 +1,47 @@
 <?php
 
+require_once('./loginManager.php');
+
+$_POST = json_decode(file_get_contents('php://input', true), true);
+
+if (isset($_POST['action']) && isset($_POST['item'])) {
+
+  switch ($_POST['action']) {
+    case 'delete':
+      deleteEntryById($_POST['item']['id']);
+    break;
+
+    case 'create':
+      # code...
+    break;
+  }
+
+  //echo json_encode('Data recibida');
+}
+
+function deleteEntryById($id) {
+
+  // Verify the file path exist
+  $jsonPath = '../../resources/employees.json';
+  if (!file_exists($jsonPath)) die('Invalid path');
+
+  // Get JSON and update it
+  $jsonArray = getJson($jsonPath);
+  $newJson = [];
+  foreach($jsonArray as $entry) {
+    if ($entry['id'] !== $id) {
+      $newJson[] = $entry;
+    }
+  }
+
+  if (!file_put_contents($jsonPath, json_encode($newJson, JSON_PRETTY_PRINT))) {
+    die('Failed to save data into json file.');
+  }
+
+  echo json_encode('Updated succesfully!');
+}
+
+/*
 $name = $_POST["name"];
 $lastName = $_POST["lastName"];
 $email = $_POST["email"];
@@ -10,3 +52,4 @@ $city = $_POST["city"];
 $street = $_POST["streetAddress"];
 $state = $_POST["state"];
 $zipCode = $_POST["zipCode"];
+*/
