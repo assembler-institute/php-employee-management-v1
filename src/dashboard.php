15f5d3c112bfbf9aefc6e3e5e8fa44151f839346
CarlosVelilla <carlosvelillaf@hotmail.com>
2021-11-11 16:14:13 +0100

    Add header content

--- src/dashboard.php
+++ src/dashboard.php
@@ -9,6 +9,8 @@
     <title>Dashboard</title>
     <script type="module" src="../assets/js/index.js" defer></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="icon" href="./favicon.svg">
+    <link rel="stylesheet" href="../assets/css/main.css">
     <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jsgrid/1.5.3/jsgrid.min.css" />
     <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jsgrid/1.5.3/jsgrid-theme.min.css" />
 </head>
