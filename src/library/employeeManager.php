ae91251b910ec3793706ff86fa54c79ffe2367c3
sergi92 <45389112+sergi92@users.noreply.github.com>
2021-11-12 10:01:54 +0100

    add function completed

--- src/library/employeeManager.php
+++ src/library/employeeManager.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * EMPLOYEE FUNCTIONS LIBRARY
  *
@@ -8,32 +9,32 @@
 
 function addEmployee(array $newEmployee)
 {
-// TODO implement it
+    // TODO implement it
 
 }
 
 
 function deleteEmployee(string $id)
 {
-// TODO implement it
+    // TODO implement it
 }
 
 
 function updateEmployee(array $updateEmployee)
 {
-// TODO implement it
+    // TODO implement it
 }
 
 
 function getEmployee(string $id)
 {
-// TODO implement it
+    // TODO implement it
 }
 
 
 function removeAvatar($id)
 {
-// TODO implement it
+    // TODO implement it
 }
 
 
