12027fd8ddb975ab4ac711ec8d6af0f08a64d211
tamtran <tamtran2885@gmail.com>
2021-11-14 00:55:43 +0100

    add session control time

--- src/dashboard.php
+++ src/dashboard.php
@@ -4,6 +4,13 @@ session_start();
 if (!isset($_SESSION["name"])) {
     header("Location: ./../index.php");
     exit;
+} else {
+    if(time()-$_SESSION["login_time_stamp"] > 600)
+    {
+        session_unset();
+        session_destroy();
+        header("Location: ./../index.php");
+    }
 }
 
 ?>
--- src/employee.php
+++ src/employee.php
@@ -5,6 +5,13 @@ session_start();
 if (!isset($_SESSION["name"])) {
     header("Location: ./../index.php");
     exit;
+} else {
+    if(time()-$_SESSION["login_time_stamp"] > 600)
+    {
+        session_unset();
+        session_destroy();
+        header("Location: ./../index.php");
+    }
 }
 
 if (isset($_GET["id"])) {
