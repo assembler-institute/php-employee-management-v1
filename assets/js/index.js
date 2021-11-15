41e619d405ab54498198117282e3e12d7a765a0d
Haroon Ahmad <haroon334@gmail.com>
2021-11-15 14:18:21 +0100

    Changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,15 +13,14 @@ document.addEventListener("DOMContentLoaded", function (event) {
 });
 
 function processEditEmployee(e) {
-
   e.preventDefault();
   let formData = new FormData(e.target);
   // Display the key/value pairs
-  let item = {}
-  for(var entry of formData.entries()) {
-    let key = entry[0]
-    let value = entry[1]
-    item[key] = value
+  let item = {};
+  for (var entry of formData.entries()) {
+    let key = entry[0];
+    let value = entry[1];
+    item[key] = value;
   }
 
   makeRequest(item, "update");
@@ -50,10 +49,11 @@ function makeRequest(item, action) {
     headers: {
       "Content-Type": "application/json",
     },
-  }).then((res) => res.json())
-  .then(msg => {
-    if (action === 'update') window.location.replace(`dashboard.php`);
   })
+    .then((res) => res.json())
+    .then((msg) => {
+      if (action === "update") window.location.replace(`dashboard.php`);
+    });
 }
 
 function setupJsGrid(data) {
@@ -62,13 +62,14 @@ function setupJsGrid(data) {
     height: "400px",
 
     inserting: true,
-    editing: false,
+    editing: true,
     sorting: true,
     paging: true,
 
     onItemInserted: insertNewEntry,
     onItemDeleted: deleteEntry,
     rowClick: editEntry,
+    onItemEditing: editEntry,
 
     deleteConfirm: "Do you really want to delete the employee?",
 
@@ -116,50 +117,55 @@ function setupJsGrid(data) {
 }
 
 // Timer for inactivity
-function activityWatcher(){
-
+function activityWatcher() {
   //The number of seconds that have passed
   //since the user was active.
   var secondsSinceLastActivity = 0;
 
   //Five minutes. 60 x 5 = 300 seconds.
-  var maxInactivity = (60 * 10);
+  var maxInactivity = 60 * 10;
 
   //Setup the setInterval method to run
   //every second. 1000 milliseconds = 1 second.
-  setInterval(function(){
-      secondsSinceLastActivity++;
-      console.log(secondsSinceLastActivity + ' seconds since the user was last active');
-      //if the user has been inactive or idle for longer
-      //then the seconds specified in maxInactivity
-      if(secondsSinceLastActivity > maxInactivity){
-          console.log('User has been inactive for more than ' + maxInactivity + ' seconds');
-          //Redirect them to your logout.php page.
-          window.location.replace(`../index.php?logout=timeout`);
-      }
+  setInterval(function () {
+    secondsSinceLastActivity++;
+    console.log(
+      secondsSinceLastActivity + " seconds since the user was last active"
+    );
+    //if the user has been inactive or idle for longer
+    //then the seconds specified in maxInactivity
+    if (secondsSinceLastActivity > maxInactivity) {
+      console.log(
+        "User has been inactive for more than " + maxInactivity + " seconds"
+      );
+      //Redirect them to your logout.php page.
+      window.location.replace(`../index.php?logout=timeout`);
+    }
   }, 1000);
 
   //The function that will be called whenever a user is active
-  function activity(){
-      //reset the secondsSinceLastActivity variable
-      //back to 0
-      secondsSinceLastActivity = 0;
+  function activity() {
+    //reset the secondsSinceLastActivity variable
+    //back to 0
+    secondsSinceLastActivity = 0;
   }
 
   //An array of DOM events that should be interpreted as
   //user activity.
   var activityEvents = [
-      'mousedown', 'mousemove', 'keydown',
-      'scroll', 'touchstart', 'click'
+    "mousedown",
+    "mousemove",
+    "keydown",
+    "scroll",
+    "touchstart",
+    "click",
   ];
 
   //add these events to the document.
   //register the activity function as the listener parameter.
-  activityEvents.forEach(function(eventName) {
-      document.addEventListener(eventName, activity, true);
+  activityEvents.forEach(function (eventName) {
+    document.addEventListener(eventName, activity, true);
   });
-
-
 }
 
 activityWatcher();
--- src/library/sessionHelper.php
+++ src/library/sessionHelper.php
@@ -1,7 +1,8 @@
 <?php
 
 # Create login Session -> index.php
-function startSession($email) {
+function startSession($email)
+{
   session_start();
 
   $_SESSION['user_email'] = $email;
@@ -10,19 +11,21 @@ function startSession($email) {
 }
 
 # Verify 
-function verifyLogin() {
+function verifyLogin()
+{
   session_start();
 
   $logged = isset($_SESSION) && isset($_SESSION['user_email']) ? true : false;
 
   if (!$logged) {
-    header('Location: ../../index.php');
+    header('Location: ../index.php');
     exit();
   }
 }
 
 # Destroy user Session
-function destroySession() {
+function destroySession()
+{
   // Inicializar la sesión.
   // Si está usando session_name("algo"), ¡no lo olvide ahora!
   session_start();
@@ -33,10 +36,15 @@ function destroySession() {
   // Si se desea destruir la sesión completamente, borre también la cookie de sesión.
   // Nota: ¡Esto destruirá la sesión, y no la información de la sesión!
   if (ini_get("session.use_cookies")) {
-      $params = session_get_cookie_params();
-      setcookie(session_name(), '', time() - 42000,
-      $params["path"], $params["domain"],
-      $params["secure"], $params["httponly"]
+    $params = session_get_cookie_params();
+    setcookie(
+      session_name(),
+      '',
+      time() - 42000,
+      $params["path"],
+      $params["domain"],
+      $params["secure"],
+      $params["httponly"]
     );
   }
 
@@ -45,5 +53,3 @@ function destroySession() {
     session_destroy();
   }
 }
-
-
