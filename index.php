967a1c561deeedbae6d0ba54203e84922c8d930d
AntonioCopete <antcopjim@gmail.com>
2021-11-15 12:55:22 +0100

    fixed import routes from login.php to index.php change

--- index.php
+++ index.php
@@ -28,8 +28,8 @@ if (getSessionValue("user")) {
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Employee Manager - Form</title>
-	<link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css" />
-	<script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js" type="module"></script>
+	<link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css" />
+	<script src="./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js" type="module"></script>
 </head>
 
 <body>
