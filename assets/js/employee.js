4ae87b7468bdeb78099fb667d614fe539bc59c79
AntonioCopete <antcopjim@gmail.com>
2021-11-16 10:34:51 +0100

    fixed navbar and employee.php refresh after update

--- assets/js/employee.js
+++ assets/js/employee.js
@@ -1,43 +1,42 @@
 import { displayNotification } from "./utils.js";
 
 (function setSubmitEvent() {
-	const form = document.querySelector("#employee-form");
+  const form = document.querySelector("#employee-form");
 
-	form.addEventListener("submit", function (event) {
-		event.preventDefault();
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-		const form = event.target;
-		const data = Object.fromEntries(new FormData(form));
-		const method = data.id ? "PUT" : "POST";
-		console.log(data, method);
+    const form = event.target;
+    const data = Object.fromEntries(new FormData(form));
+    const method = data.id ? "PUT" : "POST";
 
-		fetch("./library/employeeController.php", {
-			method,
-			body: JSON.stringify(data, null, 2),
-		})
-			.then((response) => {
-				return response.json();
-			})
-			.then((data) => {
-				displayNotification(data);
-				form.reset();
-			})
-			.catch((error) => {
-				displayNotification(error);
-			});
-	});
+    fetch("./library/employeeController.php", {
+      method,
+      body: JSON.stringify(data, null, 2),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        displayNotification(data);
+        if (method === "POST ") form.reset();
+      })
+      .catch((error) => {
+        displayNotification(error);
+      });
+  });
 })();
 
 (function setAvatarUpdate() {
-	document.addEventListener("click", function (event) {
-		const userAvatar = document.querySelector("#user-avatar");
-		const inputAvatar = document.querySelector("#avatar");
+  document.addEventListener("click", function (event) {
+    const userAvatar = document.querySelector("#user-avatar");
+    const inputAvatar = document.querySelector("#avatar");
 
-		if (event.target.matches(".avatar-btn")) {
-			const newAvatar = event.target.children[0];
+    if (event.target.matches(".avatar-btn")) {
+      const newAvatar = event.target.children[0];
 
-			userAvatar.src = newAvatar.src;
-			inputAvatar.value = newAvatar.src;
-		}
-	});
+      userAvatar.src = newAvatar.src;
+      inputAvatar.value = newAvatar.src;
+    }
+  });
 })();
