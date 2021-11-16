08c57f0f67654ab0d02603896f8190b33a54451f
CYBER-MARCUSSEN <83762741+CYBER-MARCUSSEN@users.noreply.github.com>
2021-11-16 11:17:23 +0100

    messages with toastify

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,10 +8,67 @@ $("#jsGrid").jsGrid({
   paging: true,
   autoload: true,
 
-  pageSize: 15,
+  pageSize: 10,
   pageButtonCount: 5,
 
-  deleteConfirm: "Do you really want to delete the client?",
+  deleteConfirm: "Do you really want to delete the employee?",
+
+  onItemDeleted: () => {
+
+    Toastify({
+      text: "The item has been succesfully deleted!",
+      position: "center",
+      duration: 3000,
+      close: true,
+      style: {
+        background: "linear-gradient(to right, #CB356B, #BD3F32)",
+        color: "white"
+      },
+
+      offset: {
+        x: 50, // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+        y: 10, // vertical axis - can be a number or a string indicating unity. eg: '2em'
+      },
+    }).showToast();
+  },
+
+  onItemInserted: () => {
+
+    Toastify({
+      text: "The employee has been successfully added",
+      position: "center",
+      duration: 3000,
+      close: true,
+      style: {
+        background: "linear-gradient(to right, #1CB5E0, #000046)",
+        color: "white"
+      },
+
+      offset: {
+        x: 50, // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+        y: 10, // vertical axis - can be a number or a string indicating unity. eg: '2em'
+      },
+    }).showToast();
+  },
+
+  onItemUpdated: () => {
+
+    Toastify({
+      text: "The employee has been successfully modified",
+      position: "center",
+      duration: 3000,
+      close: true,
+      style: {
+        background: "linear-gradient(to right, #34e89e, #0f3443)",
+        color: "white"
+      },
+
+      offset: {
+        x: 50, // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+        y: 10, // vertical axis - can be a number or a string indicating unity. eg: '2em'
+      },
+    }).showToast();
+  },
 
   controller: {
     loadData: function (response) {
@@ -108,6 +165,6 @@ $("#jsGrid").jsGrid({
       align: "center",
       validate: "required",
     },
-    { type: "control", editButton: false },
+    { type: "control", editButton: true },
   ],
 });
--- src/dashboard.php
+++ src/dashboard.php
@@ -14,6 +14,8 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">  
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
 
 </head>
 <body>
@@ -21,8 +23,17 @@
 // require("../src/library/sessionHelper.php");
 require(".././assets/html/header.html");
 ?>
+<br>
 
 <div id="jsGrid" class="container"></div>
+
+<!-- <?php 
+      if(isset($_GET['deletedItem'])){
+          echo "<div class='alert alert-danger'> <h5>Delete item successful</h5></div>";
+          // header('../src/dashboard.php?deletedItem');
+          // unset($_GET['deletedItem']);
+        }
+?> -->
 
 
 
