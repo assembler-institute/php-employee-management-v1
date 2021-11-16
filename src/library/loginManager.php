3ab6a3cf313dc2d9a88c79a81014b78f1cd7b166
ParisArcos <84467828+ParisArcos@users.noreply.github.com>
2021-11-16 09:15:12 +0000

    merged

--- src/library/loginManager.php
+++ src/library/loginManager.php
@@ -1,1 +1,227 @@
+
+
+
+<?php
 // This file will contain the necessary functions so that the user can log in, save their session data and log out.
+
+/**
+ * This function must first check where are we
+ *
+ * If we are on index and we are already logged it must redirect
+ * to the dashboard, if not it must check for login errors, login info or logouts
+ *
+ * If we are on dashboard it must check that we are already logged, if not
+ * it must redirect us to the index and show an error
+ */
+
+/**
+ * Checks state of session
+ * @return alert if session has ended || redirects in case its needed.
+ *
+ */
+function checkSession() {
+  # check where we are in base of the URI
+    $urlFile = basename($_SERVER['REQUEST_URI'], '?' . $_SERVER['QUERY_STRING']);
+
+      if ($urlFile === "index.php") {
+
+          if (isset($_SESSION["email"])) {
+            header("Location: ./src/dashboard.php");
+        } else {
+            // Check for session error
+            if ($alert = checkLoginError()) return $alert;
+
+            // Check for logout
+            if ($alert = checkLogout()) return $alert;
+        }
+        # else if the file is dashboard.php
+      } else {
+
+        checkLoginDashboard($urlFile);
+
+        }
+
+}
+
+
+/**
+ * This function must get input form values and check them
+ * If user is correct we must redirect user to the private area
+ */
+function authUser()
+
+{
+  $email = $_POST["email"];
+  $pass = $_POST["pass"];
+
+  if(validateEmail($email)){
+
+    if (checkUser($email, $pass)) {
+
+      // registers the initial time when the session was created
+      $_SESSION['startTime'] = $_SERVER['REQUEST_TIME'];
+
+      // keeps a record of the data email of the client and sends to dashboard
+      $_SESSION["email"] = $email;
+      header("Location: ../dashboard.php");
+
+    } else {
+      $_SESSION["errorMessage"] = "Email or Password error. Please try Again";
+      header("Location:../../index.php");
+    }
+
+  } else {
+    $_SESSION["errorMessage"] = "Email format error";
+      header("Location: ../../index.php");
+  }
+
+}
+
+/**
+ * This function must emulate a database user search and return
+ * true in case email and password matches
+ * @param
+ */
+function checkUser(string $email, string $pass)
+
+{
+    //traer de JSON
+    $emailDb = "admin@assemblerschool.com";
+    $passDb = "123456";
+
+    $passDbEnc = password_hash($passDb, PASSWORD_DEFAULT); //traer de JSON
+
+    if ($email === $emailDb && password_verify($pass, $passDbEnc)) return true;
+
+    else return false;
+}
+
+/**
+ * This function must unset all session and cookies variables
+ * and also destroy the session itself
+ */
+function destroySession()
+{
+  session_start();
+  /**
+   * Destroy session
+   */
+   $notMessage=false;
+
+   if($_SESSION['expired']) {
+      $notMessage = true;
+    };
+
+
+    session_unset();
+
+    destroySessionCookie();
+    session_destroy();
+
+    if($notMessage) {
+
+    header("Location:../../index.php?expired=true");
+
+    } else {
+
+    header("Location:../../index.php?logout=true");}
+
+
+}
+
+/**
+ * This function is used to delete session Cookie
+ */
+function destroySessionCookie()
+{
+  if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(
+        session_name(),
+        '',
+        time() - 90000,
+        $params["path"],
+        $params["domain"],
+        $params["secure"],
+        $params["httponly"]
+    );
+}
+}
+
+/**
+ * This function is used to check for login errors
+ */
+function checkLoginError()
+{
+  if (isset($_SESSION["errorMessage"])) {
+
+    $errorText = $_SESSION["errorMessage"];
+
+    # delete the var from the session
+    unset($_SESSION["errorMessage"]);
+
+    return ["status" => "warning", "message" => $errorText];
+}
+
+}
+
+function checkLoginDashboard($urlFile){
+
+  if (!isset($_SESSION["email"])) {
+
+    if(isset($_GET["expired"])){
+      $_SESSION["errorMessage"]=["status" => "warning", "message" => "Your session have ended"];
+    }
+    else{
+      $_SESSION["errorMessage"] = ["status" => "warning", "message" =>"You don't have permission to enter the dashboard. Please Login."];
+    }
+    $_SESSION['isRedirecting'] = true ;
+    $_SESSION['prevPage'] = $urlFile;
+    header("Location: ../index.php");
+  }
+}
+
+function checkRedirection(){
+
+  unset($_SESSION['isRedirecting']);
+  unset($_SESSION['prevPage']);
+
+  $error= $_SESSION["errorMessage"];
+  unset($_SESSION["errorMessage"]);
+
+
+  return $error;
+
+}
+
+
+function checkLogout()
+{
+  if(!isset($_SESSION["email"])) {
+
+    if (isset($_GET["logout"])  ){
+
+      return ["status" => "success", "message" => "You have logged out correctly"];
+
+    } elseif (isset($_GET["expired"])){
+
+      return ["status" => "success", "message" => "Your session have ended"];}
+  }
+}
+
+
+function getUserFromEmail($email){
+
+  return explode("@",$email)[0];
+
+}
+
+function validateEmail($email) {
+
+  if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+
+    return true;
+
+  }
+}
+
--- src/library/sessionHelper.php
+++ src/library/sessionHelper.php
@@ -1,1 +1,61 @@
-// This file will check that the user session is still active and if not, you must call the corresponding function of "loginManager.php" to logout the user. In the event that the user remains more than 10 minutes in the session, the user will have to be logged out.
+
+<?php
+// This file will check that the user session is still active and if not, you must call the corresponding function of "loginManager.php" to logout the user.
+// In the event that the user remains more than 10 minutes in the session, the user will have to be logged out.
+
+session_start();
+/*
+* If a key its pressed or mouse moved then it puts to cero a count down, like a set timeout. We can do it in jvs.
+* if the timer gets to 10 mins triggers and AJAX that ejectures a php file that ends session and redirects to index.
+*/
+require_once ("loginManager.php");
+/**
+* This functions calculates Session Time
+* @param initialTime (int )time when the session was initialized
+* @return sessionTimer (int) - returns time of the session in ms
+**/
+function timer(int $intialTime){
+    $sessionTimer = $_SERVER['REQUEST_TIME'] - $intialTime;
+    return $sessionTimer;
+  };
+
+/**
+* This functions sets a timeout that if achieved sends a form to the client
+* @param initialTime (int )time when the session was initialized
+* @return sessionTimer (int) - returns time of the session in ms
+**/
+function initializeTimer(){
+  // time when session was created
+  return date('Y-m-d H:i:s', $_SESSION['startTime']);
+};
+
+// send initial session time
+if(filter_input(INPUT_POST,'json_getTime')){
+
+  $json = filter_input(INPUT_POST,'json_getTime');
+  $decoded_json = json_decode($json);
+  $start = $decoded_json->start;
+
+  if($start) echo json_encode(initializeTimer());
+
+}
+
+// if event on client timer to zero
+if (filter_input(INPUT_POST,'json_endSession')){
+
+  $jsonEnd = filter_input(INPUT_POST,'json_endSession');
+  $decoded_jsonEnd = json_decode($jsonEnd);
+  $end_session = $decoded_jsonEnd->end_session;
+
+  if($end_session) {
+    $_SESSION['expired'] = true;
+    destroySession();
+  }
+}
+
+
+if(isset($_GET["expired"]) && $_GET["expired"]) destroySession();
+
+
+
+
