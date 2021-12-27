8249df89f666356e87cf01ba410153cb8b56456e
sergiogt93 <sergio.ber0556@gmail.com>
2021-12-27 23:54:20 +0100

    Apply design login

--- index.php
+++ index.php
@@ -9,35 +9,32 @@
     <link rel="stylesheet" href="./assets/css/login.css">
 </head>
 
-<body>
-    <main>
-        <div class="head">
-            <h1>Log in</h1>
-        </div>
-        <div class="log">
-            <form action="./src/library/loginManager.php" method="POST">
-                <div class="mb-3">
-                    <label for="log" class="form-label">Username</label>
-                    <input type="text" id="log" name="log">
-                </div>
-                <div class="mb-3">
-                    <label for="pass" class="form-label">Password</label>
-                    <input type="password" id="pass" name="pass">
-                </div>
+<body class="text-center">
+    <main class="form-signin">
+        <form>
+            <h3 class="mb-3">PLEASE LOGIN</h3>
+
+            <div class="form-floating mb-3">
+                <input type="email" class="form-control" id="login-name">
+                <label for="login-name">Email address or username</label>
+            </div>
+            <div class="form-floating">
+                <input type="password" class="form-control" id="login-pass">
+                <label for="login-pass">Password</label>
+            </div>
+
+            <div class="mb-3">
                 <?php require_once "./src/library/sessionHelper.php";
-                if (isset($_GET["error"])) {
-                    echo "<p class='alert alert-danger'>Password or Username is incorrect.<p>";
-                } else if (isset($_GET["notLogged"])){
-                    echo "<p class='alert alert-danger'>Access denied. Please log in.<p>";
-                }
-                indexLogCheck();
+                    if (isset($_GET["error"])) {
+                        echo "<p class='alert alert-danger'>Password or Username is incorrect.<p>";
+                    } else if (isset($_GET["notLogged"])){
+                        echo "<p class='alert alert-danger'>Access denied. Please log in.<p>";
+                    }
+                    indexLogCheck();
                 ?>
-                <div class="sub">
-                    <button type="submit" class="btn btn-primary">Log in</button>
-                </div>
-        </div>
+            </div>
+            <button class="w-100 btn btn-lg btn-primary" type="submit">SIGN IN</button>
         </form>
     </main>
 </body>
-
 </html>
