4c28bc561772fc528dd4beceeb9cb4869be39530
Raul Lopez <raul.lopez@singularfactory.net>
2021-12-31 14:41:27 +0000

    WIP grid

--- src/dashboard.php
+++ src/dashboard.php
@@ -1,1 +1,29 @@
-<!-- TODO Main view or Employees Grid View here is where you get when logged here there's the grid of employees -->
+<!-- CSS -->
+<style>
+<?php require("../assets/css/main.css"); ?>
+</style>
+
+<!-- jQuery -->
+<script type="module" src="../node_modules/jquery/src/jquery.js"></script>
+
+<!-- jsGrid -->
+<link type="text/css" rel="stylesheet" href="../node_modules/jsgrid/css/jsgrid.css" />
+<link type="text/css" rel="stylesheet" href="../node_modules/jsgrid/css/theme.css" />
+<script type="module" src="../node_modules/jsgrid/dist/jsgrid.min.js"></script>
+
+<!-- JS -->
+<script type="module" src="../assets/js/index.js" defer></script>
+
+<!-- Header -->
+<?php require("../assets/html/header.html"); ?>
+
+<!-- Grid -->
+<div class="grid"></div>
+
+<!-- Footer -->
+<?php include("../assets/html/footer.html"); ?>
+
+
+
+
+
--- src/employee.php
+++ src/employee.php
@@ -1,1 +1,6 @@
 <!-- TODO Employee view -->
+<?php require("../assets/html/header.html"); ?>
+<?php include("../assets/html/footer.html"); ?>
+<style>
+<?php require("../assets/css/main.css"); ?>
+</style>
