b14e1df57812c17102ab94acc0273592b6cb7715
Adrián Serrano <adrianno_3@hotmail.com>
2022-01-07 13:43:11 +0100

    bootstrap nav includes footer and header

--- src/dashboard.php
+++ src/dashboard.php
@@ -1,1 +1,29 @@
-<!-- TODO Main view or Employees Grid View here is where you get when logged here there's the grid of employees -->
+<?php
+include "../assets/html/header.html"
+    ?>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+  <a class="navbar-brand" href="#">
+      <img src="../assets/img/logo.jpg" alt="" width="25">
+    </a>
+    <a class="navbar-brand" href="#">Employees Management</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Employee</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<?php
+include "../assets/html/footer.html"
+    ?>
