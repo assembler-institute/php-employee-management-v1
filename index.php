c01861e8ab7bca8bb6f8055298a4a8791c26ac6f
Carla <91063081+cicu90@users.noreply.github.com>
2022-01-07 10:07:14 +0000

    link bootstrap

--- index.php
+++ index.php
@@ -7,7 +7,11 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!--CSS BOOTSTRAP-->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link rel="stylesheet" href="./assets/css/login.css">
+  <!--JS BOOTSTRAP-->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   <script src="./assets/js/index.js" defer></script>
   <title>Php-employee-management-v1</title>
 </head>
@@ -33,12 +37,6 @@
         <?php if (!empty($loginResult)) { ?>
           <div class="text-danger"><?php echo $loginResult; ?></div>
         <?php } ?>
-      </div>
-      <div class="card-footer p-3">
-        <div class="d-flex justify-content-center">
-          <div class="text-primary">If you are a registered user,
-            login here.</div>
-        </div>
       </div>
     </div>
   </div>
