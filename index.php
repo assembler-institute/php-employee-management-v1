45012d265dc937b9ca41af6538e8660086d62c1a
Kory <50864697+Kory-ifcd0210@users.noreply.github.com>
2022-01-07 15:25:38 +0000

    password verify

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" require class="form-control" id="exampleInputPassword1" name="password">
+                <input type="password" class="form-control" id="exampleInputPassword1" name="password" required>
             </div>
             <button id="submit" type="submit" class="btn btn-warning">Login</button>
         </form>
--- src/library/loginManager.php
+++ src/library/loginManager.php
@@ -1,13 +1,22 @@
 <?php
+//acces to JSON users.json
+$dataUser = file_get_contents('./../../resources/users.json');
 
-    session_start();
-    $usermail= $_POST['usermail'];
-    $password=$_POST['password'];
 
-    if( strlen($usermail) != 0 && strlen($password) != 0){
-        $_SESSION['usermail']= $usermail;
-        echo 'sesion creada ';
+session_start();
+$usermail = $_POST['usermail'];
+$password = $_POST['password'];
+
+
+    $data = json_decode($dataUser, true); //recoger array de users.json
+    $admin = $data['users'][0]; //acceder al objeto userId=1 (es una array)
+    if ($admin['email'] == $usermail && password_verify($password , $admin['password'])) {
+        echo 'tiene acceso';
+        $_SESSION['usermail'] = $usermail;
+        header("location: ../dashboard.php");
+    } else {
+        header("location: ./../../index.php");
     }
-    header("location: ../dashboard.php");
 
-?>
+
+
