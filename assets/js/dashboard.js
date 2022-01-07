741520e2002a798e67e8a82ecd707f8d94016bd1
sergiogt93 <sergio.ber0556@gmail.com>
2022-01-07 15:35:02 +0100

    show table employeers II

---
+++ assets/js/dashboard.js
@@ -0,0 +1,51 @@
+window.addEventListener("DOMContentLoaded", async() => {
+    let employeers = await getEmployeers();
+    console.log(employeers);
+    displayTypeTasks(employeers);
+});
+
+async function getEmployeers() {
+    const response = await fetch(`./library/employeeController.php?getEmployeers`);
+    const data = await response.json();
+    return data;
+}
+
+function displayTypeTasks(employeers) {
+    $("#dashboard").jsGrid({
+        height: "auto",
+        width: "100%",
+
+        // filtering: true,
+        editing: true,
+        inserting: true,
+        sorting: true,
+        paging: true,
+
+        pageSize: 15,
+        pageButtonCount: 5,
+
+        data: employeers,
+
+        fields: [
+            { name: "name", type: "text", title: "Name"},
+            { name: "email", type: "text", title: "Email"},
+            { name: "age", type: "number", title: "Age"},
+            { name: "streetAddress", type: "number", title: "Street No." },
+            { name: "city", type: "text", title: "City" },
+            { name: "state", type: "text", title: "State" },
+            { name: "postalCode", type: "number", title: "Postal Code" },
+            { name: "phoneNumber", type: "number", title: "Phone Number" },
+            { type: "control", modeSwitchButton: true, editButton: false}
+        ],
+
+        // controller: {
+        //     insertItem: function name(item) {
+        //         return $.ajax({
+        //             type: "POST",
+        //             url: "./library/employeeController.php?addEmployee",
+        //             data: item
+        //         })
+        //     }
+        // }
+    });
+}
