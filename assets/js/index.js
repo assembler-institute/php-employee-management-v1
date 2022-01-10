b310915385881143d7f67205ad3c389f8b195bb8
rogerpuig <rogerpuigdemasa@gmail.com>
2022-01-10 20:27:33 +0100

    datos edit pasados

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,36 @@
 
 
 var tabla = document.getElementById("Grid");
-var newData
-async function rellenar(){
-    await fetch("../resources/employees.json")
-      .then(response => response.json())
-      .then(data =>{
-        console.log(data);
-        newData=data
-      });
-}
-var but=document.createElement("button");
-but.innerText="e";
-var del=document.createElement("button");
-del.innerText="d";
+
+
 
 new gridjs.Grid({
     columns: ["Id","Name","Last Name", "Email","Gender","City","Street Address","State","Age","Postal Code" ,"Phone Number","edit","delete"],
     server: {
         url: "../resources/employees.json",
-        then: data => data.map(card => [card.id, card.name, card.lastName,card.email,card.gender,card.city,card.streetAddress,card.state,card.age,card.postalCode,card.phoneNumber,but,del])
-      } 
+        then: data => data.map(card =>{
+            
+            let aedi=document.createElement("a");
+            let edi=document.createElement("button");
+            aedi.href="./employee.php?id="+card.id;
+            aedi.innerText="e";
+            edi.id=card.id;
+            edi.appendChild(aedi)
+            let del=document.createElement("button");
+            let adel=document.createElement("a");
+            adel.href=""
+            del.id=card.id;
+            adel.innerText="d"
+            del.appendChild(adel)
+            return [card.id, card.name, card.lastName,card.email,card.gender,card.city,card.streetAddress,card.state,card.age,card.postalCode,card.phoneNumber,edi,del]
+        } )
+      }
 }).render(tabla)
 
 
 
+function hola(){
 
+}
 
 
--- src/employee.php
+++ src/employee.php
@@ -1,1 +1,55 @@
-<!-- TODO Employee view -->
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    
+    <!-- TODO Employee view -->
+<?php 
+    $file="../resources/employees.json";
+    $Allusers= file_get_contents($file);
+    $userAll = json_decode($Allusers);
+    // print_r($userAll);
+    foreach($userAll as $users){
+        // foreach($users as $user){
+            if( $users->id==$_GET["id"]){
+                echo $users->id;
+                echo "</br>";
+                echo $users->name;
+                echo "</br>";
+                echo $users->lastName;
+                echo "</br>";
+                echo $users->email;
+                echo "</br>";
+                echo $users->gender;
+                echo "</br>";
+                echo $users->city;
+                echo "</br>";
+                echo $users->streetAddress;
+                echo "</br>";
+                echo $users->state;
+                echo "</br>";
+                echo $users->age;
+                echo "</br>";
+                echo $users->postalCode;
+                echo "</br>";
+                echo $users->phoneNumber;
+                echo "</br>";
+            }
+    //    }
+    }
+?>
+</body>
+</html>
