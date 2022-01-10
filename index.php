3406c0fb2fe7ae1f84c732589b12cd8079b74405
Bertinoti <jeffersonbertinoti@gmail.com>
2022-01-10 09:38:05 +0100

    require and clean

--- index.php
+++ index.php
@@ -1,18 +1,11 @@
 <!-- TODO Application entry point. Login view -->
 <?php
 session_start();
-isset($_SESSION["email"]) ? header("Location:./src/dashboard.php") : ""
+isset($_SESSION["email"]) ? header("Location:./src/dashboard.php") : "";
+
+require_once './assets/html/header.html';
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="./assets/css/login.css">
-    <title>login</title>
-</head>
+
 <body>
 <section class=" gradient-custom">
 <div class="container py-5 h-90 ">
