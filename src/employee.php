7b7018effca7b3f273bac407c7a555de691750fa
gcachonc <g.cachon.c@gmail.com>
2022-01-11 17:55:54 +0100

    fill the employee form

--- src/employee.php
+++ src/employee.php
@@ -21,26 +21,45 @@
 <body>
 <?php
   require ("./../assets/html/header.html");
+  $idvalue = $_GET["id"];
+  if(isset($idvalue)){
+    $newCollections = json_decode(file_get_contents('../resources/employees.json'), true);
+    for ($i=0; $i < count($newCollections) ; $i++) {
+      if ($newCollections[$i]['id'] == $idvalue){
+        $name = $newCollections[$i]['name'];
+        $lastName = $newCollections[$i]['lastName'];
+        $email = $newCollections[$i]['email'];
+        $gender = $newCollections[$i]['gender'];
+        $age = $newCollections[$i]['age'];
+        $streetA = $newCollections[$i]['streetAddress'];
+        $city = $newCollections[$i]['city'];
+        $state = $newCollections[$i]['state'];
+        $postalCode = $newCollections[$i]['postalCode'];
+        $phoneNum = $newCollections[$i]['phoneNumber'];
+      }
+    }
+  };
+  
   ?>
   <div class="container-employee">
 <form class="row g-3 needs-validation" novalidate>
   <div class="col-md-6">
     <label for="validationCustom01" class="form-label">Name</label>
-    <input type="text" class="form-control" id="validationCustom01" value="Mark" required>
+    <input type="text" class="form-control" id="validationCustom01" value="<?php echo $name?>" placeholder="name" required>
     <div class="valid-feedback">
       Looks good!
     </div>
   </div>
   <div class="col-md-6">
     <label for="validationCustom02" class="form-label">Last Name</label>
-    <input type="text" class="form-control" id="validationCustom02" value="Otto" required>
+    <input type="text" class="form-control" id="validationCustom02" value="<?php echo $lastName?>" placeholder = "last name" required>
     <div class="valid-feedback">
       Looks good!
     </div>
   </div>
   <div class="col-md-6">
     <label for="validationCustomEmail" class="form-label">Email Adress</label>
-    <input type="text" class="form-control" id="validationCustomEmail" value="" required>
+    <input type="text" class="form-control" id="validationCustomEmail" value="<?php echo $email?>" placeholder = "email direction" required>
     <div class="valid-feedback">
       Looks good!
     </div>
@@ -48,9 +67,9 @@
   <div class="col-md-6">
     <label for="validationCustomGender" class="form-label">Gender</label>
     <select class="form-select" id="validationCustomGender" required>
-      <option selected disabled value="">Gender....</option>
-        <option>man</option>
-        <option>woman</option>
+      <option <?php if(!isset($idvalue)) {echo "selected";} if($gender === ""){echo "selected";} ?> disabled value="">Gender....</option>
+        <option <?php if($gender == "man") echo "selected"?>>man</option>
+        <option <?php if($gender == "woman") echo "selected"?>>woman</option>
         <option>Not Gender</option>
     </select>
     <div class="invalid-feedback">
@@ -59,45 +78,42 @@
   </div>
   <div class="col-md-6">
     <label for="validationCustom03" class="form-label">City</label>
-    <input type="text" class="form-control" id="validationCustom03" required>
+    <input type="text" class="form-control" id="validationCustom03" value="<?php echo $city?>" placeholder = "city" required>
     <div class="invalid-feedback">
       Please provide a valid city.
     </div>
   </div>
   <div class="col-md-6">
     <label for="validationCustomSaddress" class="form-label">Street Address</label>
-    <input type="text" class="form-control" id="validationCustomSaddress" required>
+    <input type="text" class="form-control" id="validationCustomSaddress" value="<?php echo $streetA?>" placeholder = "street address"required>
     <div class="invalid-feedback">
-      Please provide a valid city.
+      Please provide a valid address.
     </div>
   </div>
   <div class="col-md-6">
-    <label for="validationCustom04" class="form-label">State</label>
-    <select class="form-select" id="validationCustom04" required>
-      <option selected disabled value="">Choose...</option>
-      <option>...</option>
-    </select>
+    <label for="validationCustomSaddress" class="form-label">State</label>
+    <input type="text" class="form-control" id="validationCustomSaddress" value="<?php echo $state?>" placeholder = "State"required>
     <div class="invalid-feedback">
-      Please select a valid state.
+      Please provide a valid State.
     </div>
   </div>
   <div class="col-md-6">
     <label for="validationCustomAge" class="form-label">Age</label>
-    <input type="text" class="form-control" id="validationCustomAge" required>
+    <input type="text" class="form-control" id="validationCustomAge" value="<?php echo $age?>" placeholder = "Age" required>
     <div class="invalid-feedback">
-      Put at age!.
+      Type an age.
     </div>
   </div>
   <div class="col-md-6">
     <label for="validationCustom05" class="form-label">Postal Code</label>
-    <input type="text" class="form-control" id="validationCustom05" required>
+    <input type="text" class="form-control" id="validationCustom05" value="<?php echo $postalCode?>" placeholder = "Postal Code" required>
     <div class="invalid-feedback">
       Please provide a valid Postal Code.
     </div>
   </div>
   <div class="col-md-6">
     <label for="validationCustom06" class="form-label">Phone Number</label>
-    <input type="text" class="form-control" id="validationCustom06" required>
+    <input type="text" class="form-control" id="validationCustom06" value="<?php echo $phoneNum?>" placeholder = "Phone number" required>
     <div class="invalid-feedback">
       Please provide a valid Phone Number.
     </div>
@@ -112,6 +128,7 @@
 </div>
 <?php
   require ("./../assets/html/footer.html");
+  
   ?>
 </body>
 </html>
