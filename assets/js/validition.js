d2ae7c5e2ea09f2babfd6604075112e317c0e99b
jseo123 <jose123135@gmail.com>
2022-01-11 15:57:48 +0100

    Added validation warnigs

--- assets/js/validition.js
+++ assets/js/validition.js
@@ -18,30 +18,59 @@ document.getElementById("postalCode").addEventListener("blur", (e)=> {
     validatePostalCode(e.target);
     })
 
+    let i = 0;
 function validateSurName(inputValidation) {
     let valueInput = inputValidation.value.trim();
-    return (valueInput.length < 20 && valueInput.length > 3) ? true : false;
+    let val = (valueInput.length < 20 && valueInput.length > 3) ? true : false;
+    validate(val)
+    return val
 }
 
 function validateEmail(inputValidation) {
     const EMAIL_PATTERN = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
     let emailValue = inputValidation.value.trim();
-    return (emailValue.match(EMAIL_PATTERN) && emailValue.length < 51) ? true : false;
+    let val = (emailValue.match(EMAIL_PATTERN) && emailValue.length < 51) ? true : false;
+    validate(val)
+    return val
 }
 
 function validateIsAge(inputValidation) {
     var ageValue = inputValidation.trim();
-    return (!isNaN(age)) ? true : false;
+    let val =(!isNaN(age)) ? true : false;
+    validate(val)
+    return val
 }
 
 function validatePhone(inputValidation){
 let phoneInput = inputValidation.value.trim();
-return (phoneInput.length === 9) ? true : false
-
+let val = (phoneInput.length === 9) ? true : false
+validate(val)
+return val
 }
 
 function  validatePostalCode(inputValidation){
 let postalCodeInput = inputValidation.value.trim();
-return (postalCodeInput.length === 5) ? true : false
+let val = (postalCodeInput.length === 5) ? true : false
+validate(val)
+return val
+}
 
+function validate(val){
+if (val === false && i === 0){
+    i++
+    let form =  document.getElementById("formulary")
+    let warning = document.createElement("p")
+    warning.innerText = "Please enter the correct data"
+    warning.setAttribute("class", "bg-danger")
+    warning.setAttribute("id", "danger")
+    warning.style.color = "white"
+    form.appendChild(warning)
+} else if ( val === true){
+    if (i > 0){
+        i--
+       if(document.getElementById("danger")){
+           document.getElementById("formulary").removeChild(document.getElementById("danger"))
+       }
+    }
+}
 }
--- src/employee.php
+++ src/employee.php
@@ -8,7 +8,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="styleSheet" href="../assets/css/main.css">
-    <!-- <script defer src="../assets/js/validition.js"></script> -->
+<script defer src="../assets/js/validition.js"></script>
 </head>
 
 <body>
@@ -23,10 +23,9 @@
         $employee = getEmployee($_GET["editEmployee"]);
         var_dump($employee);
     }
-    sessionCheck();
     ?>
     <main>
-        <form class="employeeForm" action="./library/employeeController.php" method="POST">
+        <form class="employeeForm" action="./library/employeeController.php" method="POST" id="formulary">
             <div class="formFlex">
                 <div class="flexMember">
             <label for="name">Name</label><br>
@@ -84,7 +83,7 @@
             </div>
             <div class="formFlex">
             <div class="flexButtons">
-                <button type="submit" name="addEmployee" class="btn btn-primary">submit</button>
+                <button type="submit" name="addEmployee" class="btn btn-primary" id="formSub">submit</button>
             <a href="dashboard.php"><button type="button" class="btn btn-secondary">Return</button></a>
             </div>
             </div>
