47f796df889562195590977502e96cbcea8793b8
Carla <91063081+cicu90@users.noreply.github.com>
2022-01-11 15:34:36 +0000

    add input and css employee

--- src/employee.php
+++ src/employee.php
@@ -8,6 +8,7 @@
    <!--CSS BOOTSTRAP-->
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
    <link rel="stylesheet" href="../assets/css/main.css">
+   <link rel="stylesheet" href="../assets/css/employee.css">
    <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jsgrid/1.5.3/jsgrid.min.css" />
    <!--JS BOOTSTRAP-->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous" defer></script>
@@ -21,29 +22,30 @@
 <?php
   require ("./../assets/html/header.html");
   ?>
+  <div class="container-employee">
 <form class="row g-3 needs-validation" novalidate>
-  <div class="col-md-4">
+  <div class="col-md-6">
     <label for="validationCustom01" class="form-label">Name</label>
     <input type="text" class="form-control" id="validationCustom01" value="Mark" required>
     <div class="valid-feedback">
       Looks good!
     </div>
   </div>
-  <div class="col-md-4">
+  <div class="col-md-6">
     <label for="validationCustom02" class="form-label">Last Name</label>
     <input type="text" class="form-control" id="validationCustom02" value="Otto" required>
     <div class="valid-feedback">
       Looks good!
     </div>
   </div>
-  <div class="col-md-4">
-    <label for="validationCustomEmail" class="form-label">Email</label>
+  <div class="col-md-6">
+    <label for="validationCustomEmail" class="form-label">Email Adress</label>
     <input type="text" class="form-control" id="validationCustomEmail" value="" required>
     <div class="valid-feedback">
       Looks good!
     </div>
   </div>
-  <div class="col-md-2">
+  <div class="col-md-6">
     <label for="validationCustomGender" class="form-label">Gender</label>
     <select class="form-select" id="validationCustomGender" required>
       <option selected disabled value="">Gender....</option>
@@ -55,20 +57,6 @@
       Please select a Gender.
     </div>
   </div>
-  <div class="col-md-2">
-    <label for="validationCustomAge" class="form-label">Age</label>
-    <input type="text" class="form-control" id="validationCustomAge" required>
-    <div class="invalid-feedback">
-      Put at age!.
-    </div>
-  </div>
-  <div class="col-md-6">
-    <label for="validationCustomSaddress" class="form-label">streetAddress</label>
-    <input type="text" class="form-control" id="validationCustomSaddress" required>
-    <div class="invalid-feedback">
-      Please provide a valid city.
-    </div>
-  </div>
   <div class="col-md-6">
     <label for="validationCustom03" class="form-label">City</label>
     <input type="text" class="form-control" id="validationCustom03" required>
@@ -76,7 +64,14 @@
       Please provide a valid city.
     </div>
   </div>
-  <div class="col-md-3">
+  <div class="col-md-6">
+    <label for="validationCustomSaddress" class="form-label">Street Address</label>
+    <input type="text" class="form-control" id="validationCustomSaddress" required>
+    <div class="invalid-feedback">
+      Please provide a valid city.
+    </div>
+  </div>
+  <div class="col-md-6">
     <label for="validationCustom04" class="form-label">State</label>
     <select class="form-select" id="validationCustom04" required>
       <option selected disabled value="">Choose...</option>
@@ -86,17 +81,35 @@
       Please select a valid state.
     </div>
   </div>
-  <div class="col-md-3">
-    <label for="validationCustom05" class="form-label">Zip</label>
-    <input type="text" class="form-control" id="validationCustom05" required>
+  <div class="col-md-6">
+    <label for="validationCustomAge" class="form-label">Age</label>
+    <input type="text" class="form-control" id="validationCustomAge" required>
     <div class="invalid-feedback">
-      Please provide a valid zip.
+      Put at age!.
     </div>
   </div>
-  <div class="col-12">
-    <button class="btn btn-primary" type="submit">Submit form</button>
+  <div class="col-md-6">
+    <label for="validationCustom05" class="form-label">Postal Code</label>
+    <input type="text" class="form-control" id="validationCustom05" required>
+    <div class="invalid-feedback">
+      Please provide a valid Postal Code.
+    </div>
+  </div>
+  <div class="col-md-6">
+    <label for="validationCustom06" class="form-label">Phone Number</label>
+    <input type="text" class="form-control" id="validationCustom06" required>
+    <div class="invalid-feedback">
+      Please provide a valid Phone Number.
+    </div>
+  </div>
+  <div class="col-2">
+    <button class="btn btn-primary" type="submit">Submit</button>
+  </div>
+  <div class="col-2">
+    <button class="btn btn-secondary" type="submit">Return</button>
   </div>
 </form>
+</div>
 <?php
   require ("./../assets/html/footer.html");
   ?>
