c3393030be2c10a0a1e642741378d76b67fd8c53
Albert Garcia Fernandez <alberd_beto@hotmail.com>
2022-01-12 10:44:55 +0100

    1st version logout

--- src/library/loginController.php
+++ src/library/loginController.php
@@ -25,3 +25,23 @@ function startSession()
         $_SESSION["login_time"] = time();
         header("Location:../dashboard.php");
 }
+
+function deleteSession()
+{
+    unset($_SESSION);
+    // destroy session cookie
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(
+            session_name(),
+            '',
+            time() - 42000,
+            $params["path"],
+            $params["domain"],
+            $params["secure"],
+            $params["httponly"]
+        );
+    }
+    session_destroy();
+    header("location:./../index.php");
+}
