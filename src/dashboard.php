71b134e411b76b8cb408a0cd8875a7253772c4b2
Bertinoti <jeffersonbertinoti@gmail.com>
2022-01-13 21:33:14 +0100

    start again

--- src/dashboard.php
+++ src/dashboard.php
@@ -5,31 +5,40 @@ session_start();
 require '../assets/html/header.html';
 require "././library/loginManager.php";
 
-if(isset($_POST)){
-    
+if (isset($_POST)) {
+
     // sessionlogout("Location:../index.php");
 }
 
- if(isset($_SESSION['email'])){
+if (isset($_SESSION['email'])) {
     echo $_SESSION['email'];
     echo "<script> loadxml(); </script>";
 }
 
 ?>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<link rel="stylesheet" href="./../node_modules/bootstrap/dist/css/bootstrap.min.css">
+<!-- todo import from CDN and Others -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Luxurious+Roman&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+    integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
 <script src="./../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 
-<body onload=" callGrid(); ">
-    <div class="row">
-        <h5 id="loggedUser">
-        </h5>
-        <form action="./library/loginController.php" method="post">
-            <button type="submit" name="logout">Log out </button>
-        </form>
-    </div>
 
-    <div id="jsGrid"> </div>
+<body onload=" callGrid(); ">
+    <?php
+        require '../assets/html/navbar.html';
+    ?>
+
+    <section>
+        <div id="jsGrid"> </div>
+    </section>
+
+    <?php
+        require '../assets/html/footer.html';
+    ?>
+
 </body>
-<!-- <form action="../index.php" method="post"></form> -->
+
 </html>
