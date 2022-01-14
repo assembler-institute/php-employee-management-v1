6844cd7c1c7f7403ef7675c8c5301bbc7c098948
Bertinoti <jeffersonbertinoti@gmail.com>
2022-01-14 02:17:37 +0100

    done

--- src/employee.php
+++ src/employee.php
@@ -8,22 +8,20 @@ if (isset($_GET['id'])) {
 }
 ?>
 <?php
-    // require './library/employeeManager.php';
-    if (isset($_POST['submitForm'])) {
-    $seemail= recorrer("../resources/employees.json", $_POST['email']);
+// require './library/employeeManager.php';
+if (isset($_POST['submitForm'])) {
+    $seemail = recorrer("../resources/employees.json", $_POST['email']);
     // echo $prueba;
-    if (isset($_GET['id']) ) {
-        if($_POST["lastName"] != " " && $_POST["radio"] != " "){
+    if (isset($_GET['id'])) {
+        if ($_POST["lastName"] != " " && $_POST["radio"] != " ") {
             updateEmployee($_POST, "../resources/employees.json");
-
         }
         print_r($_POST);
-    } else if($seemail){
+    } else if ($seemail) {
         echo "this email is already in use";
-    }else{
+    } else {
         addEmployee("../resources/employees.json");
         header("Location:./dashboard.php");
-
     }
 }
 ?>
@@ -37,7 +35,7 @@ if (isset($_GET['id'])) {
     <div class="wrapper rounded bg-white">
         <div class="h3">Registration Form</div>
         <form action="" method="POST">
-        <div class="form">
+            <div class="form">
                 <div class="row">
                     <div class="col-md-6 mt-md-0 mt-3"> <label>First Name</label> <input type="text" class="form-control" name="name" required value="<?= isset($_GET["id"]) ?  name : "" ?>"> </div>
                     <div class="col-md-6 mt-md-0 mt-3"> <label>Last Name</label> <input type="text" class="form-control" name="lastName" required value="<?= isset($_GET["id"]) ?  nmeLast : "" ?>"> </div>
@@ -45,7 +43,7 @@ if (isset($_GET['id'])) {
                 <div class="row">
                     <div class="col-md-6 mt-md-0 mt-3"> <label>Email</label> <input type="email" class="form-control" name="email" required value="<?= isset($_GET["id"]) ?  email : "" ?>"> </div>
                     <div class="col-md-6 mt-md-0 mt-3"> <label>Gender</label>
-                    <?php
+                        <?php
                         if (isset($_GET['id'])) {
                             if (gender == "man") {
                                 echo "<div class='d-flex align-items-center mt-2'> <label class='option'> <input type='radio' name='radio' value='man' checked required>Male <span class='checkmark'></span> </label> <label class='option ms-4'> <input type='radio' name='radio' value='woman' required>Female <span class='checkmark'></span> </label> </div>";
@@ -57,9 +55,11 @@ if (isset($_GET['id'])) {
                         } else {
                             echo "<div class='d-flex align-items-center mt-2'> <label class='option'> <input type='radio' name='radio' value='man'  required>Male <span class='checkmark'></span> </label> <label class='option ms-4'> <input type='radio' name='radio' value='female' required>Female <span class='checkmark'></span> </label> </div>";
                         }
-                        ?></div>
+                        ?>
+                    </div>
+                </div>
                     <div class="row">
-                        <div class="col-md-6 mt-md-0 mt-3"> <label>City</label> <input type="text" class="form-control" name="city" required value="<?= isset($_GET["id"]) ?  city : ''?>"> </div>
+                        <div class="col-md-6 mt-md-0 mt-3"> <label>City</label> <input type="text" class="form-control" name="city" required value="<?= isset($_GET["id"]) ?  city : '' ?>"> </div>
                         <div class="col-md-6 mt-md-0 mt-3"> <label>Street Adress</label> <input type="text" class="form-control" name="streetAddress" required value="<?= isset($_GET["id"]) ?  streetAdress : '' ?>"> </div>
                     </div>
                     <div class="row">
@@ -68,17 +68,19 @@ if (isset($_GET['id'])) {
                     </div>
                     <!-- <div class="row d-flex justify-content-around"> -->
                     <div class="row">
-                        <div class="col-md-6 mt-md-0 mt-3"> <label>Postal Code</label> <input type="number" class="form-control" name="postalCode" required value="<?=isset($_GET["id"])?postalCode : ''?>"> </div>
-                        <div class="col-md-6 mt-md-0 mt-3"> <label>Phone Number</label> <input type="tel" class="form-control" name="phoneNumber" required value="<?=isset($_GET['id']) ?  phoneNumber : ''?>"> </div>
+                        <div class="col-md-6 mt-md-0 mt-3"> <label>Postal Code</label> <input type="number" class="form-control" name="postalCode" required value="<?= isset($_GET["id"]) ? postalCode : '' ?>"> </div>
+                        <div class="col-md-6 mt-md-0 mt-3"> <label>Phone Number</label> <input type="tel" class="form-control" name="phoneNumber" required value="<?= isset($_GET['id']) ?  phoneNumber : '' ?>"> </div>
                     </div>
                     <div class="row d-flex justify-content-around">
-<button class="col-5 dflex btn btn-primary btn-lg" type="submit" name="submitForm"> Submit </button>
-
-<button class="col-5 btn btn-secondary btn-lg " id="buttonCancel" > <a href="./dashboard.php">Cancel</a> </button>
-
-</form>
-</div>
-        </div>
+                        <div class='col-5'>
+                            <button class=" btn btn-primary btn-lg" type="submit" name="submitForm"> Submit </button>
+                        </div>
+        </form>
+                        <div class='col-5'>
+                              <button class=" btn btn-secondary btn-lg " id="buttonCancel" onclick="window.location.href='./dashboard.php'"> Cancel </button>
+                        </div>
+                    </div>
+            </div>
     </div>
 </body>
 
