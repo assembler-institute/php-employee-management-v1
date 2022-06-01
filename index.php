64e65b1d2de01a69e8be64c1705149709f0f303c
Jesus Herrera <heisusin@gmail.com>
2022-06-01 09:42:46 +0200

    created login form

--- index.php
+++ index.php
@@ -1,1 +1,31 @@
 <!-- TODO Application entry point. Login view -->
+<?php
+
+require_once
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="./src/library/loginController.php" method="post">
+        <div>
+            <label for="">Email address</label>
+            <input type="email" name="email" id="">
+        </div>
+        <div>
+            <label for="">Password</label>
+            <input type="password" name="pass" id="">
+        </div>
+        <button type="submit" name="login" value="login">Sign in</button>
+    </form>
+    
+</body>
+</html>
