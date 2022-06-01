c378744ec387925254b1cccd173fd7db3fc31dbd
reduansj <reduansenoun@gmail.com>
2022-06-01 14:48:43 +0200

    feature: Html structure

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
 <!-- TODO Application entry point. Login view -->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <main>
+      <section class="login__container">
+        <form id="loginForm" class="login__form">
+          <input type="text" class="form__input" name="userEmail" />
+          <input type="text" class="form__input" name="loginPassword" />
+          <button class="primary__btn" id="loginFormSubmitBtn">LOGIN</button>
+        </form>
+      </section>
+    </main>
+  </body>
+</html>
--- src/dashboard.php
+++ src/dashboard.php
@@ -1,1 +1,57 @@
 <!-- TODO Main view or Employees Grid View here is where you get when logged here there's the grid of employees -->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <header>
+      <div id="logo" class="logo__container">
+        <img src="" alt="" />
+      </div>
+      <h1>EMPLOYEE MANAGEMENT</h1>
+    </header>
+    <nav>
+      <div class="links__container">
+        <span id="dashboardLink" class="links__container--item">DASHBOARD</span>
+        <span id="employeeLink" class="links__container--item">EMPLOYEE</span>
+      </div>
+      <div class="logout__btn_container">
+        <button id="logoutBtn" class="secondary__btn">LOGOUT</button>
+      </div>
+    </nav>
+    <main>
+      <sectiion class="dashboard__table__containeer">
+        <table class="dashboard__employee__table">
+          <tr class="dashboard__employe__th">
+            <th>NAME</th>
+            <th>EMAIL</th>
+            <th>AGE</th>
+            <th>STREET No.</th>
+            <th>CITY</th>
+            <th>STATE</th>
+            <th>POSTAL CODE</th>
+            <th>PHONE NUMBER</th>
+            <th>
+              <button id="dashboardAddNewEmployee">+</button>
+            </th>
+          </tr>
+          <tr id="employeeID" class="dashboard__employee__td">
+            <td>Juan</td>
+            <td>juan@huan.com</td>
+            <td>22</td>
+            <td>xxxx</td>
+            <td>xxxxx</td>
+            <td>xxxxx</td>
+            <td>xxxxx</td>
+            <td>666666666</td>
+            <td><button id="dashboardRemoveEmployee">DELETE</button></td>
+          </tr>
+        </table>
+      </sectiion>
+    </main>
+  </body>
+</html>
