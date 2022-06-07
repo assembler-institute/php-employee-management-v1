52563f990739799f01a07fe39dcb5bc593497275
Jesus Herrera <heisusin@gmail.com>
2022-06-07 22:56:43 +0200

    added styles to login page

--- index.php
+++ index.php
@@ -14,27 +14,32 @@ $alert = checkSession();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="./assets/css/login.css">
     <title>Document</title>
 </head>
 <body>
-
-    <form action="./src/library/loginController.php" method="post">
-        <div>
-            <label for="">Email address</label>
-            <input type="email" name="email" id="">
-        </div>
-        <div>
-            <label for="">Password</label>
-            <input type="password" name="pass" id="">
-        </div>
-        <button type="submit" name="login" value="login">Sign in</button>
-
-        <?php
-        echo $alert;
-
-        ?>
-
-    </form>
+    <div class="login__container--box">
+        <h2 class="login__container--title">LOGIN</h2>
+            <form class="login__form" action="./src/library/loginController.php" method="post">
+                <div class="user__input">
+                    <label for="">Email address</label>
+                    <input type="email" name="email" id="">
+                    <i class='bx bxs-user icon__user--log'></i>
+                </div>
+                <div class="user__input">
+                    <label for="">Password</label>
+                    <input type="password" name="pass" id="">
+                    <i class='bx bxs-lock icon__pass--log' ></i>
+                </div>
+                <div class="login__user--container">
+                    <button class="login__user--button" type="submit" name="login" value="login">Sign in</button>
+                </div>
+                <?php
+                echo $alert;
+                ?>
+            </form>
+    </div>
     
 </body>
 </html>
