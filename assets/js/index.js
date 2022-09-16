0c16970cf70061db5bf4afe98341f78d56b97db4
AlejandroLHC <alejandrolhc@gmail.com>
2022-09-16 15:35:38 +0200

    Login access and restriction access to content

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,13 +11,25 @@ form.addEventListener('submit', e => {
     })
         .then(res => res.json())
         .then(data => { 
-            let alert = document.createElement('span');
-            alert.innerHTML = data;
-            alert.className = 'alert alert-danger';
-            while(alertBox.hasChildNodes()){
-                alertBox.removeChild(alertBox.firstChild);
-            };
-            alertBox.appendChild(alert);
+            if (data === 'You need to fill in all the information') {
+                let alert = document.createElement('span');
+                alert.innerHTML = data;
+                alert.className = 'alert alert-danger';
+                while(alertBox.hasChildNodes()){
+                    alertBox.removeChild(alertBox.firstChild);
+                };
+                alertBox.appendChild(alert);
+            } else if (data === 'OK') {
+                window.location.replace("http://localhost/assembler_projects/php_module/php-employee-management-v1/src/dashboard.php");
+            } else if (data === 'Error: Not matching data found') {
+                let noMatch = document.createElement('span');
+                noMatch.innerHTML = data;
+                noMatch.className = 'alert alert-danger';
+                while(alertBox.hasChildNodes()){
+                    alertBox.removeChild(alertBox.firstChild);
+                };
+                alertBox.appendChild(noMatch);
+            }
         });
 });
 
--- src/library/loginManager.php
+++ src/library/loginManager.php
@@ -1,12 +1,25 @@
 <?php 
-    require_once('resources/users.json');
+    function accessAdmin($name, $pass) {
+        session_start();
+
+        $adminsInfo = json_decode(file_get_contents('../../resources/users.json'),true)['users'];
     
-    function accessUser($name, $pass) {
-        if(TODO MAL) {
-
-            return 'The server has thrown an unkown error.<br>Please, try again later.'
+        if ($name === '' || $pass === ''){
+            echo json_encode('You need to fill in all the information');       
         } else {
-
-            return 'wiiii'
+            foreach ($adminsInfo as $admin) {
+                if (($name === $admin['name']) || 
+                    ($name === $admin['email'])) {
+                    if (password_verify($pass, $admin['password'])){
+                        echo json_encode('OK');
+                        $adminId = $admin['userId'];
+                        $_SESSION['user'] = $adminId;
+                    } else {
+                        echo json_encode('Error: Not matching data found');
+                    }
+                } else {
+                    echo json_encode('Error: Not matching data found');
+                }
+            }
         }
     }
