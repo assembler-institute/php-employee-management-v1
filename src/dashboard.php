3793c469b16250b6364bdae7739e6ffe45c33521
tonymg1 <tonymartingaldeano@gmail.com>
2022-09-19 09:28:00 +0200

    m

--- src/dashboard.php
+++ src/dashboard.php
@@ -6,6 +6,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <!-- JavaScript Bundle with Popper -->
@@ -28,6 +30,7 @@
                 <th scope="col">Phone State</th>
                 <th scope="col">Phone Postal Code</th>
                 <th scope="col">Phone Number</th>
+                <th scope="col"><i class="uil uil-plus"></i></th>    
             </tr>
         </thead>
         <tbody>
