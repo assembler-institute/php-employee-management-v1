51c188d8db97b110ae70088943cf923120d53cb5
Alejandro L. Herrero <alejandrolhc@gmail.com>
2022-09-19 10:02:37 +0200

    Template header footer v.1

--- src/dashboard.php
+++ src/dashboard.php
@@ -12,14 +12,20 @@
     <title>Login</title>
     <script src="assets/js/index.js" defer></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/main.css" >
+    <link rel="stylesheet" href="../assets/css/main.css" >
   </head>
   <body>
+    <header>
+      <?php require_once('../assets/html/header.html') ?>
+    </header>
     <main>
-        <div>
-            Esto es el Dashboard
-        </div>
+      <div>
+        Esto es el Dashboard
+      </div>
     </main>
+    <footer>
+      <?php require_once('../assets/html/footer.html') ?>
+    </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
   </body>
 </html>
